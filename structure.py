2c4bcf1694a0e66f2d2578936bf2e081c548220c
Evelyn Fitzgerald <acf67@cornell.edu>
2020-12-06 17:54:45 -0500

    Finish describing model parameters

--- structure.py
+++ structure.py
@@ -12,10 +12,13 @@ class StructureData:
     - Z[l, i, a] (int): the origin of the `a`th allele copy at locus `l` in individual `i`
     - P[k, l, j] (float): the frequency of allele `j` at locus `l` in population `k`
     - Q[k, i] (float): the fraction of individual `i`'s ancestry from population `k`
-    - r (float): the number of generations since admixture
-    - F[k] (float): TODO figure out what these variables mean
-    - alpha
-    - lamda
+    - r (float): the number of generations since admixture; also the rate parameter for the
+      linkage Markov model.
+    - F[k] (float): the genetic drift rate for population `k`, assuming that all populations
+      diverged from a single ancestral population at the same time (the F model)
+    - alpha[k] (float): the relative contribution of population `k` to the genetic material
+    - lamda[j] (float): parameters to the Dirichlet distribution for the hypothetical ancestral
+      population in the F model.
     '''
     def __init__(self, X, K) -> None:
         '''
