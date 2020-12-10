052f996e2dae4442250fabe0d9c2d14483fad709
Evelyn Fitzgerald <acf67@cornell.edu>
2020-12-10 16:26:47 -0500

    Doing STRUCTURE 1.0 now

--- linkage_model.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-from structure import StructureData
-
-def iterate_linkage_model(struct: StructureData):
-    pass
--- structure.py
+++ structure.py
@@ -2,23 +2,18 @@ import numpy as np
 
 class StructureData:
     '''
-    An object that stores the variables used in STRUCTURE 2.0.
+    An object that stores the variables used in STRUCTURE 1.0.
 
     Inputs:
     - X[l, i, a] (int): the genotype of allele copy `a` at locus `l` for individual `i`
-    - K (int): the number of ancestral populations
+    - K (int): the number of populations
 
     Want to estimate:
     - Z[l, i, a] (int): the origin of the `a`th allele copy at locus `l` in individual `i`
     - P[k, l, j] (float): the frequency of allele `j` at locus `l` in population `k`
     - Q[k, i] (float): the fraction of individual `i`'s ancestry from population `k`
-    - r (float): the number of generations since admixture; also the rate parameter for the
-      linkage Markov model.
-    - F[k] (float): the genetic drift rate for population `k`, assuming that all populations
-      diverged from a single ancestral population at the same time (the F model)
-    - alpha[k] (float): the relative contribution of population `k` to the genetic material
-    - lamda[j] (float): parameters to the Dirichlet distribution for the hypothetical ancestral
-      population in the F model.
+    - alpha (float): a parameter to the underlying Dirichlet distribution. Indicates the
+      degree of admixture in the populations.
     '''
     def __init__(self, X: np.ndarray, K: int) -> None:
         '''
