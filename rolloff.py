e0cb436584b45ae1130e1218e4912a67d4cd48a5
Evelyn Fitzgerald <acf67@cornell.edu>
2020-12-12 00:53:13 -0500

    Start num_generations estimator Convert genetic_distance to calculate distances between all loci Start binning algorithm

--- rolloff.py
+++ rolloff.py
@@ -3,12 +3,20 @@ from numpy.core.fromnumeric import prod
 from scipy.stats import pearsonr
 from itertools import product
 
-def genetic_distance(l1: int, l2: int) -> float:
+def genetic_distance(pos: np.ndarray) -> np.ndarray:
     '''
-    Computes the genetic distance in morgans between the loci at positions `l1` and `l2`.
+    Computes the genetic distance in morgans between all pairs of loci in `pos`.
+
+    Parameters:
+    - pos[l] (float): the position of locus `l` in bps
+
+    Returns:
+    - dist[l1, l2] (float): the genetic distance between `l1` and `l2` in morgans
     '''
-    # One centimorgan = about 1 million bp
-    return np.abs(l1 - l2) / 1e8
+    # Compute the physical distances between all pairs of loci
+    dist_bps = np.abs(pos[np.newaxis, :] - pos[:, np.newaxis])
+    # Convert to genetic distance in morgans (1 centimorgan = about 1 million bp)
+    return dist_bps / 1e8
 
 def weight(P: np.ndarray, k1: int = 0, k2: int = 1) -> np.ndarray:
     '''
@@ -17,6 +25,9 @@ def weight(P: np.ndarray, k1: int = 0, k2: int = 1) -> np.ndarray:
     Parameters:
     - P[k, l, j] (float): the frequency of allele `j` at locus `l` in population `k`
     - k1, k2 (int): two populations chosen for this function. Default: 0, 1
+
+    Returns:
+    - w[l] (float): the weight function for locus `l`
     '''
     # Caution: Each P[k, l, :] sums to 1
     # Choose the alleles with the highest frequency in each population at each locus
@@ -48,3 +59,33 @@ def ld_score(X: np.ndarray) -> np.ndarray:
     z_transform = np.arctanh(corr)
     # Multiply by sqrt(N - 3) and return
     return np.sqrt(N - 3) * z_transform
+
+def bin_by_distance(dist: np.ndarray, w_prod: np.ndarray, z: np.ndarray):
+    '''
+    Sorts all pairs of loci by genetic distance, then bins them 
+    '''
+    # Sort all pairs of loci using dist as a sort key
+    dist, w_prod, z = dist.flatten(), w_prod.flatten(), z.flatten()
+    sort_indices = dist.argsort()
+    dist, w_prod, z = dist[sort_indices], w_prod[sort_indices], z[sort_indices]
+    # Bin size = 0.1 cM
+    bin_size = 0.1e-2
+
+
+def num_generations(X: np.ndarray, P: np.ndarray, pos: np.ndarray, k1: int = 0, k2: int = 1) -> float:
+    '''
+    Estimates the number of generates since admixture between populations `k1` and `k2`.
+
+    Parameters:
+    - X[l, i, a] (int): the genotype of allele copy `a` at locus `l` for individual `i`
+    - P[k, l, j] (float): the frequency of allele `j` at locus `l` in population `k`
+    - k1, k2 (int): two populations chosen for this function. Default: 0, 1
+    '''
+    # Compute weights, LD scores, and distances for all loci
+    w = weight(P, k1, k2)
+    z = ld_score(X)
+    dist = genetic_distance(pos)
+    # Compute the outer product of w with itself, i.e. w(l1) * w(l2) for all l1, l2
+    w_prod = np.outer(w, w)
+    # Sort all cells by genetic distance
+    bin_by_distance(dist, w_prod, z)
